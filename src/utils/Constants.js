9f0463eff6d9e7020da32c42718a3755de45f287
Jesus Martin <jesusmarzor28@gmail.com>
2022-05-28 17:46:55 +0200

    Añadir variables, traducciones y logo

---
+++ src/utils/Constants.js
@@ -0,0 +1,122 @@
+export const LANGUAGES = {
+    "EN": 'en',
+}
+
+export const TRANSACTIONS = [
+    {
+        id: 1,
+        number: 35,
+        description: "Last Month"
+    },
+    {
+        id:2,
+        number: 3,
+        description: "Today"
+    }
+
+]
+
+export const OPTIONS_PIE = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'bottom'
+      },
+      title: {
+        display: false,
+      },
+    },
+};
+export const DATA_PIE = [
+    {
+        label: 'Phones',
+        value: 20,
+        color: "#9013FE"
+    },
+    {
+        label: 'Screens',
+        value: 40,
+        color: '#007AFF'
+    },
+    {
+        label: 'PCs',
+        value: 10,
+        color: '#FB8832'
+    },
+    {
+        label: 'Others',
+        value: 30,
+        color: '#E6E5E6'
+    }
+];
+
+export const OPTIONS_LINE = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top',
+      },
+      title: {
+        display: false,
+      },
+    },
+};
+export const DATA_WEEKLY_EXPENSES = [
+    {
+        label: 'Mon',
+        value: 10000
+    },
+    {
+        label: 'Tue',
+        value: 20000
+    },
+    {
+        label: 'Wed',
+        value: 8000
+    },
+    {
+        label: 'Thu',
+        value: 34000
+    },
+    {
+        label: 'Fri',
+        value: 2000
+    },
+    {
+        label: 'Sat',
+        value: 30000
+    },
+    {
+        label: 'Sun',
+        value: 14000
+    },
+];
+export const DATA_TRANSACTION_HISTORY = [
+    {
+        label: 'Jan',
+        value: 25
+    },
+    {
+        label: 'Feb',
+        value: 27
+    },
+    {
+        label: 'Mar',
+        value: 20
+    },
+    {
+        label: 'Apr',
+        value: 28
+    },
+    {
+        label: 'May',
+        value: 26
+    },
+    {
+        label: 'Jun',
+        value: 24
+    }
+];
+export const COLOR_LINE = '#2D14C4';
