eac05c0c73659c84016a1661dcb5ea2bcf9a54ad
Jesus Martin <jesusmarzor28@gmail.com>
2022-05-28 17:50:04 +0200

    Crear la unica pagina del proyecto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Dashboard } from "../../components/Dashboard"
+import { Header } from "./../../components/Header"
+
+export const Home = () => {
+    return (
+        <>
+            <Header/>
+            <Dashboard/>
+        </>
+    )
+}
